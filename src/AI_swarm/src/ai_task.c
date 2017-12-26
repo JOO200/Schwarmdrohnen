9f26d0769e01a4e0a3fd5ce027ab05812b5294bd
NSchoofs <nicolaischoofs@web.de>
2017-12-26 15:17:36 +0100

    Kommentare mit Erklärung eingefügt und Fehler bei include beseitigt

--- src/AI_swarm/src/ai_task.c
+++ src/AI_swarm/src/ai_task.c
@@ -1,4 +1,4 @@
-#include "ai_launch.h"
+#include "ai_task.h"
 
 /* FreeRtos includes */
 #include "FreeRTOS.h"
@@ -6,9 +6,10 @@
 
 #define AI_TASK_NAME "ai_task"
 #define AI_TASK_STACKSIZE 1024  //kp was genug/zu viel - MIST
-#define AI_TASK_PRIO 40			//hier einfaches aber schlechtes tuning möglich - MIST
+#define AI_TASK_PRIO 40			//hier einfaches aber evtl. schlechtes tuning möglich - MIST
 
 
+//wird in initphase von main in main.c aufgerufen, bevor scheduler gestartet wird
 void ai_launch(void)
 {
 	xTaskCreate(ai_Task, AI_TASK_NAME,		
@@ -17,11 +18,12 @@ void ai_launch(void)
 }
 
 //hier unsere main
+//wird ausgeführt von FreeRTOS-Scheduler sobald dieser das für sinnvoll hält (und natürlich,nachdem dieser in "main.c" gestartet wurde)
 void ai_Task(void * arg) {
-
+	//... lokale Vars, function calls
 
 	while (1) {
-		//...
+		//... repetetives
 	}
 }
 
