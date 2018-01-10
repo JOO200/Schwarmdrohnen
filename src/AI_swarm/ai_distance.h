3cf496434bc1ea4995953a055caa9ad2e96757c5
JLaier <33932111+JLaier@users.noreply.github.com>
2018-01-10 12:28:01 +0100

    Meeting 180110
    Beschreibung Funktion ai_distance.c

---
+++ src/AI_swarm/ai_distance.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "ai_config.h"
+#include "ai_locodeck.h"
+#include "ai_datatypes.h"
+#include "stddef.h"	
+
+// Sorgt für Aktualisierung der Distanztabellen in jeder Drohen, unabhängig von der Rolle der Drohne
+void UpdateDistanceTable();
