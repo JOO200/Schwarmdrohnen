e49674d622d94152b010a25461bdb5f24d3e1d09
NSchoofs <nicolaischoofs@web.de>
2018-03-15 14:36:44 +0100

    Funktion ShowDistance in ledring12.c

--- src/deck/drivers/src/ledring12.c
+++ src/deck/drivers/src/ledring12.c
@@ -141,7 +141,11 @@ static const uint8_t blueRing[][3] = {{64, 64, 255}, {32,32,64}, {8,8,16},
 Zeigt Distanzveraenderungen anhand Helligkeit-/Farbwechsel des LED-Ring-Boards
 */
 //static void ai_showDistance(uint8_t buffer[][3], bool reset, float AbstandInMeter)
-static void ai_showDistanceEffect(uint8_t buffer[][3], bool reset);
+static void ai_showDistanceEffect(uint8_t buffer[][3], bool reset)
+{
+		ws2812Send(buffer, NBR_LEDS);
+}
+
 void ai_showDistance(float AbstandInMeter)
 {
 	/*Aufbau:*/
@@ -159,7 +163,7 @@ void ai_showDistance(float AbstandInMeter)
 			ai_CurrentColor[i][3] = RED;
 		}
 	}
-	ws2812Send(ai_CurrentColor, NBR_LEDS);
+	ai_showDistanceEffect(ai_CurrentColor, FALSE);
 }
 
 static void whiteSpinEffect(uint8_t buffer[][3], bool reset)
