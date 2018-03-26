90d0f593fdc0010e4437858db33146186ad9f261
JOO200 <git@joo200.de>
2018-03-26 16:58:35 +0200

    Estimatorfix

--- src/AI_swarm/estimator_ai.c
+++ src/AI_swarm/estimator_ai.c
@@ -1,5 +1,5 @@
-#include "..\modules\src\estimator_kalman.h"
-#include "..\modules\src\estimator_kalman.c"
+#include "estimator_kalman.h"
+//#include "..\modules\src\estimator_kalman.c"
 
 #include "stm32f4xx.h"
 #include "FreeRTOS.h"
@@ -13,97 +13,105 @@
 #include "math.h"
 #include "arm_math.h"
 
-#define HYSTERESE_RATE 0.1;        // Hysterese-Rate bei 0.1 Hz => 10 sec
+static uint32_t hystereseTickWaitTime = 30000;        // Hysterese-Rate bei allen 30k Ticks
 
 static control_t * main_control;
 
-static bool isInit = FALSE;
-static int32_t lastHysterese;
+static uint8_t isAiEstimatorInit = 0;
+static uint32_t lastHysterese;
 
-static bool onGround = FALSE;
-static bool masterOnGround = FALSE;
+static uint8_t masterOnGround = 0;
+
+void EstimatorAi_Nothing(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
+void EstimatorAi_onGround(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
+void EstimatorAi_followingMaster(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
+void EstimatorAi_hysterese(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
+void EstimatorAi_emergencyLanding(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
 
 
 enum eEstimatorSM {
-    Estimator_not_init,
-    onGround,
-    followingMaster,
-    hysterese,
-    emergencyLanding
+    eEstimator_not_init = 0,
+    eEstimator_onGround,
+    eEstimator_followingMaster,
+    eEstimator_hysterese,
+    eEstimator_emergencyLanding,
+	eEstimator_SM_size
 };
 
-static uint8_t estimatorState = Estimator_not_init;
+void(*aiEstimatorSMfuncs[eEstimator_SM_size]) (state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) =
+		{
+			&EstimatorAi_Nothing,			/* eEstimator_not_init */
+			&EstimatorAi_onGround,			/* eEstimator_onGround */
+			&EstimatorAi_followingMaster,	/* eEstimator_followingMaster */
+			&EstimatorAi_hysterese,			/* eEstimator_hysterese */
+			&EstimatorAi_emergencyLanding	/* eEstimator_emergencyLanding */
+		};
 
-/*
- * Primary functions from
- */
-void handleStart        (state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
-void handleHysterese    (state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick);
 
+
+static uint8_t estimatorState = eEstimator_not_init;
 
 void estimatorAi(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
-    switch(estimatorState) {
-        case Estimator_not_init:
-            break;
-        case onGround:
-            if(masterOnGround) return;      // wenn der Master auch auf dem Boden ist, ist es dem Slave egal.
-            handleStart(state, sensors, control, tick);
-            break;
-        case followingMaster:
-        	uint32_t tick_diff = lastHysterese-tick;
-            if(tick_diff > ( configTICK_RATE_HZ / HYSTERESE_RATE)) {
-                estimatorState = hysterese;
-            }
-            control = main_control;
-            estimatorKalman(state, sensors, control, tick);
-            // control = main_control;
-            break;
-        case hysterese:
-            handleHysterese(state, sensors, control, tick);
-            break;
+	aiEstimatorSMfuncs[estimatorState](state, sensors, control, tick);
+}
 
+static double hystereseAngle = 0;    // Hysterese beginnt bei 0°, endet bei 360°
+static uint32_t hystereseStartTick = 0;
 
+static const double hystereseFactor = 1;       // Faktor - je größer desto größer wird die hysterese
+static const uint32_t hystereseTickTime = 5000;     // Hysterese innerhalb von 5000 Ticks fliegen.
 
-    }
-
-
-
+void EstimatorAi_Nothing(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
 
 }
 
-static float hystereseAngle = 0;    // Hysterese beginnt bei 0°, endet bei 360°
-static uint32_t hystereseStartTick = 0;
 
-static const float hystereseFactor = 1;       // Faktor - je größer desto größer wird die hysterese
-static const float hystereseTickTime = 5000;     // Hysterese innerhalb von 5000 Ticks fliegen.
+void EstimatorAi_onGround(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
+	if(masterOnGround) return;
+	estimatorKalman(state, sensors, main_control, tick);
+	estimatorState = eEstimator_followingMaster;
+}
 
-void handleHysterese(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
+
+void EstimatorAi_followingMaster(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
+	uint32_t tick_diff = lastHysterese-tick;
+	if(tick_diff > hystereseTickWaitTime) {
+		estimatorState = eEstimator_hysterese;
+	}
+	estimatorKalman(state, sensors, main_control, tick);
+}
+
+void EstimatorAi_hysterese(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
     if(hystereseStartTick == 0) {
     	hystereseAngle = 0;
     	hystereseStartTick = tick;
     }
     else hystereseAngle = ( 2 * PI * (tick-hystereseStartTick) ) / hystereseTickTime ;
 
-    estimatorKalman(state, sensors, control, tick);	// Einfach den Control von der Master-Drohne übernehmen.
-    if(hystereseAngle >= 2*PI)	// Hysterse zu Ende:
+    estimatorKalman(state, sensors, main_control, tick);	// Einfach den Control von der Master-Drohne übernehmen.
+    if(hystereseAngle >= (double)(2*PI))	// Hysterse zu Ende:
     {
-    	estimatorState = followingMaster;
+    	estimatorState = eEstimator_followingMaster;
     	return;
     }
-    state->attitude.roll += cos(hystereseAngle)*hystereseFactor;
-    state->attitude.yaw += sin(hystereseAngle)*hystereseFactor;
+    state->attitude.roll += (float) (cos(hystereseAngle)*hystereseFactor);
+    state->attitude.yaw += (float)(sin(hystereseAngle)*hystereseFactor);
 }
 
-void handleStart(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
-	estimatorKalmna(state, sensors, control, tick);
+void EstimatorAi_emergencyLanding(state_t *state, sensorData_t *sensors, control_t *control, const uint32_t tick) {
+	state->attitude.pitch = 0;
+	state->attitude.yaw = 0;
+	state->attitude.roll = 0;
+	state->attitude.timestamp = tick;
 }
 
 bool estimatorAiTest(void) {
-	return isInit;
+	return isAiEstimatorInit;
 }
 
 void estimatorAiInit(void) {
     estimatorKalmanInit();
-    estimatorState = onGround;  // sollte so sein...
+    estimatorState = eEstimator_onGround;  // sollte so sein...
+    isAiEstimatorInit = 1;
 }
 
